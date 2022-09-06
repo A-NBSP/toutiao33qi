1aca35b6e1a6070bc0f48493eeb8802017ca75ce
jr <1932933132@qq.com>
2022-09-06 16:15:44 +0800

    postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="toutiao">黑马头条</div>
+  <div class="toutiao">
+    黑马头条
+    <span class="toutiao toutiao-gengduo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">11</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped lang="less">
+  .box{
+    width: 2rem;
+    height: 2rem;
+    background-color: lightblue;
+  }
+</style>
